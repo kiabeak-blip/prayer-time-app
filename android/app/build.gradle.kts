94329a6624d9e2d7b2fced0c5e555a139a50c0c4
Imanbarkus <Imanbarkus@users.noreply.github.com>
2026-06-02 14:33:08 +0200

    Fix file_picker version, update NDK to 28.2, bump to build 14

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,7 @@ if (keyPropertiesFile.exists()) {
 android {
     namespace = "com.muslimapp.awqat"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
     
 
     compileOptions {
